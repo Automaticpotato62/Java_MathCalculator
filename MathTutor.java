955f8359b1af1d4c3a2912ebe417ac0ca4f3c635
Narumanchi Anitej <2134521@jeffcoschools.us>
2026-09-16 15:17:56 -0600

    I have created a new scanner object and wrote print statements prinitng an output, and then I called the nextLine methods with the Scanner object and stored it into a String variable

---
+++ MathTutor.java
@@ -0,0 +1,21 @@
+package Java_MathCalculator;
+/*
+ * Title: java_MathCalculator
+ * Author: Anitej Narumanchi
+ * Purpose:
+ *     code is able to run user-inputed values through equations to get answers, for 3-5 equations
+ * Resources :
+ *      
+*/
+
+ import java . util . Scanner ;
+
+ public class MathTutor{
+    public static void main ( String [] args ){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Math Tutor!");
+        System.out.println("/n Choose a problem: ");
+        System.out.println("\n\n\n 1. SquareRoot \n 2. AreaOfRectangle \n 3.NaturalLogarithm");
+        String choice = input.nextLine();
+ }
+ }
